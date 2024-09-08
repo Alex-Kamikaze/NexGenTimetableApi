9eeed2bd89a5b3af7c69ca0a14990eab573189ea
Alex <sashagameac@gmail.com>
2024-09-08 19:41:50 +0300

    Первый коммит

---
+++ timetable_api/timetable/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Group, Teacher, Timetable, Subject
+# Register your models here.
+admin.site.register(Group)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Timetable)
---
+++ timetable_api/timetable/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+
+# Create your models here.
+class Group(models.Model):
+    """ Учебная группа """
+
+    group_name = models.CharField(max_length=8, verbose_name='Название группы')
+    group_course = models.IntegerField(default=1, verbose_name='Номер курса')
+
+    class Meta:
+        verbose_name = "Учебная группа"
+        verbose_name_plural = "Учебные группы"
+
+    def __str__(self):
+        return self.group_name
+    
+    def as_json(self):
+        return dict(group_id = self.pk, group_name = self.group_name, group_course = self.group_course)
+    
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=250, verbose_name="ФИО Преподавателя")
+    teacher_is_active = models.BooleanField(default=True, verbose_name="Действующий сотрудник") #В случае увольнения - поставить False, и тогда будут внесены изменения в составление замещений для клиентов
+
+
+    class Meta:
+        verbose_name = "Преподаватель"
+        verbose_name_plural = "Преподаватели"
+
+    def __str__(self):
+        return self.teacher_name
+    
+    def as_json(self):
+        return dict(teacher_id = self.pk, teacher_name = self.teacher_name, teacher_is_active = self.teacher_is_active)
+    
+class Subject(models.Model):
+    """ Учебные дисциплины """
+    subject_name = models.CharField(max_length=120, verbose_name="Название дисциплины")
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.PROTECT, verbose_name="Преподаватель")
+
+    class Meta:
+        verbose_name = "Дисциплина"
+        verbose_name_plural = "Дисциплины"
+
+    def __str__(self):
+        return self.subject_name
+    
+    def as_json(self):
+        return dict(subject_id = self.pk, subject_name = self.subject_name)
+
+
+
+class Timetable(models.Model):
+    pair_numbers = (
+        ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4")
+    )
+
+    pair_number = models.CharField(max_length=1, choices=pair_numbers, verbose_name="Номер пары")
+    subject_id = models.ForeignKey(Subject, on_delete=models.PROTECT, verbose_name="Учебная дисциплина")
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.PROTECT, verbose_name="Преподаватель")
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Учебная группа")
+    cabinet_number = models.CharField(max_length=6, verbose_name="Номер кабинета")
+    pair_begin_time = models.TimeField(verbose_name="Время начала пары")
+    pair_end_time = models.TimeField(verbose_name="Время конца пары")
+
+    class Meta:
+        verbose_name = "Занятие"
+        verbose_name_plural = "Занятия"
+
+    def __str__(self):
+        return f"{self.pair_number} пара для группы {self.group_id.group_name}"
+    
+    # TODO: Добавить as_json
+
+    def as_json(self):
+        return dict(timetable_id = self.pk, pair_number = self.pair_number, group_id = self.group_id.pk, cabinet_number = self.cabinet_number, teacher_name = self.teacher_id.teacher_name, start_time = f"{self.pair_begin_time.hour}:{self.pair_begin_time.min}", end_time=f"{self.pair_end_time.hour}:{self.pair_end_time.min}")
