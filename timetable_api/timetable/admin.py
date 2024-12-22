ae806345ea08524ef37bf370a0387424258fb478
Alex <sashagameac@gmail.com>
2024-12-22 03:58:03 +0300

    Добавлено поле для поиска в админ панель в расписании

--- timetable_api/timetable/admin.py
+++ timetable_api/timetable/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Group, Teacher, Timetable, Subject, Substitution
 # Register your models here.
+
+class TimetableAdminModel(admin.ModelAdmin):
+    search_fields = ("group_id__group_name", "day_of_week")
+
 admin.site.register(Group)
 admin.site.register(Teacher)
 admin.site.register(Subject)
-admin.site.register(Timetable)
+admin.site.register(Timetable, TimetableAdminModel)
 admin.site.register(Substitution)
